3b57633e351100b20810dbe2a0e297ac43392ba7
Vee Han <cloud_strife@hotmail.my>
2018-11-16 22:13:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var express = require('express')
 var app = express()
 const bodyParser = require('body-parser')
 
-server.use(bodyParser.json({ verify: rawBodySaver }))
-server.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ verify: (req, res, buf, encoding) => {
+  if (buf && buf.length) {
+    req.rawBody = buf
+  }
+} }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/api/endpoint1', cors(), (req, res) => {
+app.get('/api/endpoint1', (req, res) => {
     res.send(JSON.stringify({hi: "Hello World newest cors done"}));
 })
 
