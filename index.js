891ac275d383ae9afd8a619ed4a689fa42d097e2
Vee Han <cloud_strife@hotmail.my>
2018-11-16 22:00:58 +0800

    cors

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require('express')
 var app = express()
-
+var cors = require('cors')
 
 app.use(function (req, res, next) {
     res.header('Content-Type', 'application/json');
     next();
 });
 
-app.get('/api/endpoint1', (req, res) => {
-    res.send(JSON.stringify({hi: "Hello World newest"}));
+app.get('/api/endpoint1', cors(), (req, res) => {
+    res.send(JSON.stringify({hi: "Hello World newest cors done"}));
 })
 
 app.listen(process.env.PORT || 80)
+var express = require('express')
+var cors = require('cors')
+var app = express()
