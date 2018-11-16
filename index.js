c71aa7a6445dde9f0e6c787d89c04fcbf7d66329
Vee Han <cloud_strife@hotmail.my>
2018-11-16 21:34:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World newest')
+  res.send('Hello World')
 })
 
-app.listen(80)
+app.listen(process.env.PORT || 80)
