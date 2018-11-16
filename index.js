61c0fa0830a965ecb4f5f29445c3059ec6373359
Vee Han <cloud_strife@hotmail.my>
2018-11-16 21:22:19 +0800

    Init

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(process.env.PORT || 8080)
+console.log(`running on ${process.env.PORT || 8080}`)
